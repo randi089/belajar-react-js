eecb2a87b7b229a57e1692ed1106712a31f999fc
Randi Febriadi <randifebriadi@gmail.com>
2025-01-30 19:31:26 +0700

    selesai

--- belajar-react-dasar/vite.config.js
+++ belajar-react-dasar/vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
         timer: "timer.html",
         guestbook: "guestbook.html",
         product: "product.html",
+        online: "online.html",
       },
     },
   },
